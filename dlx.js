bac91c51eff180ee74a021e112aa690e580b8f11
Johannes Laire <johannes@laire.fi>
2012-04-01 22:50:20 +0300

    implement LinkedMatrix

--- dlx.js
+++ dlx.js
@@ -40,7 +40,106 @@ var dlx = (function() {
 		return dense;
 	};
 
+	var Node = function(x, y) {
+		this.x = x;
+		this.y = y;
+
+		this.size = 0;
+
+		this.l = this;
+		this.r = this;
+		this.u = this;
+		this.d = this;
+	};
+
+	Node.prototype.hide_lr = function() {
+		this.l.r = this.r;
+		this.r.l = this.l;
+	};
+
+	Node.prototype.hide_ud = function() {
+		this.u.d = this.d;
+		this.d.u = this.u;
+	};
+
+	Node.prototype.show_lr = function() {
+		this.l.r = this;
+		this.r.l = this;
+	};
+
+	Node.prototype.show_ud = function() {
+		this.u.d = this;
+		this.d.u = this;
+	};
+
+	Node.prototype.set_l = function(l) {
+		l.l = this.l;
+		l.r = this;
+		this.l.r = l;
+		this.l = l;
+	};
+
+	Node.prototype.set_u = function(u) {
+		u.u = this.u;
+		u.d = this;
+		this.u.d = u;
+		this.u = u;
+	};
+
+	var LinkedMatrix = function() {
+		this.root = new Node(-1, -1);
+		this.cols = [];
+	};
+
+	LinkedMatrix.prototype.to_sparse = function() {
+		var sparse = [];
+		var put = function(x, y) {
+			while (sparse.length <= y) {
+				sparse.push([]);
+			}
+			sparse[y].push(x);
+		};
+		for (var col = this.root.r; col !== this.root; col = col.r) {
+			for (var row = col.d; row !== col; row = row.d) {
+				put(col.x, row.y);
+			}
+		}
+		for (var i = 0; i < sparse.length; ++i) {
+			sparse[i].sort();
+		}
+		return sparse;
+	};
+
+	LinkedMatrix.from_sparse = function(sparse) {
+		var lm = new LinkedMatrix();
+		var col = function(x) {
+			while (lm.cols.length <= x) {
+				var node = new Node(lm.cols.length, -1);
+				lm.root.set_l(node);
+				lm.cols.push(node);
+			}
+			return lm.cols[x];
+		};
+		for (var y = 0; y < sparse.length; ++y) {
+			var row = new Node(-1, y);
+			for (var i = 0; i < sparse[y].length; ++i) {
+				var x = sparse[y][i];
+				var node = new Node(x, y);
+				col(x).set_u(node);
+				++col(x).size;
+				row.set_l(node);
+			}
+			row.hide_lr();
+		}
+		return lm;
+	};
+
+	LinkedMatrix.from_dense = function(dense) {
+		return LinkedMatrix.from_sparse(dense_to_sparse(dense));
+	};
+
 	return {
+		'LinkedMatrix': LinkedMatrix,
 		'dense_to_sparse': dense_to_sparse,
 		'sparse_to_dense': sparse_to_dense
 	};
--- t/00_sanity.js
+++ t/00_sanity.js
@@ -2,9 +2,10 @@ var tap = require("tap");
 var dlx = require("../dlx");
 
 tap.test("dlx loaded", function(t) {
-	t.plan(3);
+	t.plan(4);
 	t.ok(dlx, "dlx exists");
 	t.ok(dlx.dense_to_sparse, "dlx.dense_to_sparse exists");
 	t.ok(dlx.sparse_to_dense, "dlx.sparse_to_dense exists");
+	t.ok(dlx.LinkedMatrix, "dlx.LinkedMatrix exists");
 	t.end();
 });
